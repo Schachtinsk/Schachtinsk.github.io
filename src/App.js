ea7c8edcc116442f85c604c63c82eb3ccd079584
Ilias <hildebrandtilias@gmail.com>
2024-09-01 21:21:47 +0200

    URL

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import "./styles/main.css"
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import "./styles/main.css";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Project from "./pages/Project";
+import Contacts from "./pages/Contacts";
 
-
-import Navbar from "./components/navbar/Navbar"
-import Footer from "./components/footer/Footer"
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import Project from "./pages/Project"
-import Contacts from "./pages/Contacts"
-
-import ScrollToTop from "./utils/scrollToTop"
-
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
-  return ( 
-  <div className="App">
-    <Router>
-      <ScrollToTop/>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/project/:id" element={<Project/>}/>
-        <Route path="/contacts" element={<Contacts/>}/>
-      </Routes>
-      <Footer />
-    </Router>
-  </div>
-  )
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/frontend-portfolio') {
+      window.location.replace('https://Schachtinsk.github.io/');
+    }
+  }, [location.pathname]);
+
+  return (
+    <div className="App">
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
